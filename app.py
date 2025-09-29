0ecf6324a2ed5174b0db5448c973741a5e02283d
Paul Frappier <paul.frappier@hotmail.fr>
2025-09-29 12:52:02 +0200

    updated procfile

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def create_app():
 
     return app
     
+# Just for local Dev
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+# if __name__ == '__main__':
+    # app = create_app()
+    #app.run(debug=True)
